40452e95c85d4cbeacb5df5652ce7b9a2aa6cdaf
TranThangBin <thangdev04@gmail.com>
2024-06-08 15:42:30 +0700

    feat: rework animation - use ansi move cursor instead of ansi clear screen to prevent dead   frame - make a fancier bomb (pretty much like the old PR) - use a collection of string with move cursor instead of multiline   string more easier to control and read - centered the animation

--- clera.py
+++ clera.py
@@ -1,128 +1,91 @@
 #!/usr/bin/env python3
 
-import time
-import sys
-import os
+from time import sleep
+from os import get_terminal_size
+from sys import stdout
+
+write = stdout.write
+flush = stdout.flush
+
+move_cursor = "\033[%d;%dH"
+move_cursor_home = move_cursor % (1, 1)
+hide_cursor = "\033[?25l"
+show_cursor = "\033[?25h"
+clear_screen = move_cursor_home + "\033[2J"
+clear_line = "\033[2K"
+
+term_width, term_height = get_terminal_size()
+horizontal_center = int(term_width / 2)
+
+spf = 0.15
+
+bomb = [
+    lambda x, y:
+           f"{move_cursor%(y,x)} *"
+         f"{move_cursor%(y+1,x)}**"
+        f"{move_cursor%(y+2,x)} *)",
+
+    lambda x, y: 
+           f"{move_cursor%(y,x)} *"
+         f"{move_cursor%(y+1,x)}**"
+        f"{move_cursor%(y+2,x)} (*",
+]
+
+frames = [
+    f"{move_cursor%(term_height,horizontal_center-8)}" r"_____\\*//______",
+
+    f"{move_cursor%(term_height-4,horizontal_center-12)}"   "            ,            "
+    f"{move_cursor%(term_height-4,horizontal_center-12)}"   "         `               "
+    f"{move_cursor%(term_height-3,horizontal_center-12)}" """     *  %  '  "    :     """
+    f"{move_cursor%(term_height-2,horizontal_center-12)}"   "     '   _____   ,       "
+    f"{move_cursor%(term_height-1,horizontal_center-12)}"  r"       '/_ - _\          "
+    f"{move_cursor%(term_height,horizontal_center-12)}"    r"_______/_||||||\_________",
+
+    f"{move_cursor%(term_height-13,horizontal_center-17)}" "          CcQ0;cCQOo0.Q            "
+    f"{move_cursor%(term_height-12,horizontal_center-17)}" "       CcQ0;cCQOo0.0oCQe.e         "
+    f"{move_cursor%(term_height-11,horizontal_center-17)}" "      CcQ0;cCQOo0.0oCQe.CcQ0;      "
+    f"{move_cursor%(term_height-10,horizontal_center-17)}" "    CcQ0;cCQOo0.0oCQe.CcQ0;cCQ     "
+    f"{move_cursor%(term_height-9,horizontal_center-17)}"  "   CcQ0;cCQOo0.0oCQe.cQ0;cCQOo0    "
+    f"{move_cursor%(term_height-8,horizontal_center-17)}"  "CcQ0;cCQOo0.0oCQe.cQ0;cCQOo0.0oCQe "
+    f"{move_cursor%(term_height-7,horizontal_center-17)}"  "   CcQ0;cCQOo0.0oCQe.CcQ0;cCQ      "
+    f"{move_cursor%(term_height-6,horizontal_center-17)}"  " ____________|_|_|_|____________   "
+    f"{move_cursor%(term_height-5,horizontal_center-17)}"  "(_______________________________)  "
+    f"{move_cursor%(term_height-4,horizontal_center-17)}"  "             |||||||               "
+    f"{move_cursor%(term_height-3,horizontal_center-17)}"  "             |||||||               "
+    f"{move_cursor%(term_height-2,horizontal_center-17)}"  "           CcQ0;cCQOo0             "
+    f"{move_cursor%(term_height-1,horizontal_center-17)}"  "          CcQ0;cCQOo0.o            "
+    f"{move_cursor%(term_height,horizontal_center-17)}"    "________CcQ0;cCQOo0.0oCQe._________",
+]
+
+
+def bomb_fall():
+    for i in range(1, term_height - 1):
+        write(
+            f"{move_cursor%(i-1,1)}{clear_line}"
+            f"{move_cursor%(i,1)}{clear_line}"
+            f"{bomb[i % 2](horizontal_center-term_height+i, i)}"
+        )
+        flush()
+        sleep(spf)
+
+
+def blow_up():
+    for frame in frames:
+        write(frame)
+        flush()
+        sleep(spf)
+
 
 def animate_clera():
-    frames = [
-        r"""
-          *
-             
-   
-   
-   
-   
-        """,
-        r"""
-            
-            *
-            
-            
-        """,
-        
-        r"""  
-        
-        
-         
-              *
-              
-              
-        """,
-
-        r"""   
-        
-        
-        
-        
-                *
-                
-                
-        """,
-        
-                r"""   
-        
-        
-        
-        
-                
-                  
-                  *
-                
-                
-        """,
-                r"""   
-        
-        
-        
-        
-                
-                
-                     
-                    *
-                
-        """,
-        r"""
-        
-        
-        
-        
-        
-        
-
-        
-        
-        
-        
-                   
-                
-                _____\\*//______
-        """,
-        r"""
-            
-            
-                 
-                 
-                 
-            
-                 
-                 
-                         ,
-                      ` 
-                  *  %  '  "    :
-                  '   _____   ,
-                    '/_ - _\         
-             _______/_||||||\_________                              
-       """,
-       r"""
-                 CcQ0;cCQOo0.Q
-              CcQ0;cCQOo0.0oCQe.e
-             CcQ0;cCQOo0.0oCQe.CcQ0;
-           CcQ0;cCQOo0.0oCQe.CcQ0;cCQ
-          CcQ0;cCQOo0.0oCQe.cQ0;cCQOo0
-       CcQ0;cCQOo0.0oCQe.cQ0;cCQOo0.0oCQe       
-          CcQ0;cCQOo0.0oCQe.CcQ0;cCQ
-        ____________|_|_|_|____________
-       (_______________________________)
-                    |||||||
-                    |||||||       
-                  CcQ0;cCQOo0
-                 CcQ0;cCQOo0.o       
-       ________CcQ0;cCQOo0.0oCQe._________
-      """
-    ]
-
     try:
-        for frame in frames:
-            clear = "\033[H\033[J"  # ANSI escape code to clear the terminal
-            sys.stdout.write(clear)
-            print(frame)
-            time.sleep(0.3)
+        write(clear_screen + hide_cursor)
+        bomb_fall()
+        blow_up()
     except KeyboardInterrupt:
-        sys.exit(0)
-        
+        return
+    finally:
+        write(show_cursor + "\n")
+
+
 if __name__ == "__main__":
     animate_clera()
-time.sleep(0.4)    
-os.system('clear')
-
